88d327e2d933cc9658cb0ff3e4b8b78899db2caa
Luis Guzman <luis.dev@lmaero.pro>
2021-06-10 17:29:15 -0500

    Complete exercise 1.5

--- part-01/02-course-information/src/App.js
+++ part-01/02-course-information/src/App.js
@@ -48,30 +48,32 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <React.StrictMode>
       <React.Fragment>
-        <Header course={course} />
+        <Header course={course.name} />
 
-        <Content parts={parts} />
+        <Content parts={course.parts} />
 
-        <Total parts={parts} />
+        <Total parts={course.parts} />
       </React.Fragment>
     </React.StrictMode>
   );
