ba5fa8bbecdb3f704fc59903d324eea9b1607f5e
Luis Guzman <luis.dev@lmaero.pro>
2021-06-12 07:15:36 -0500

    Complete exercise 1.6

---
+++ part-01/03-unicafe/src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <React.StrictMode>
+      <React.Fragment>
+        <h1>Unicafe</h1>
+        <h2>give feedback</h2>
+        <button onClick={() => setGood(good + 1)}>good</button>
+        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+        <button onClick={() => setBad(bad + 1)}>bad</button>
+
+        <h2>statistics</h2>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </React.Fragment>
+    </React.StrictMode>
+  );
+};
+
+export default App;
