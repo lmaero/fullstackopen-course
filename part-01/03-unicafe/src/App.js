051f653b4c223ac44b9d670ed363f7079c4f5bd7
Luis Guzman <luis.dev@lmaero.pro>
2021-06-12 07:29:43 -0500

    Complete exercise 1.8

--- part-01/03-unicafe/src/App.js
+++ part-01/03-unicafe/src/App.js
@@ -1,13 +1,27 @@
 import React, { useState } from 'react';
 
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
+  return (
+    <React.Fragment>
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </React.Fragment>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const all = good + neutral + bad;
-  const average = (good - bad) / all;
-  const positive = (good / all) * 100;
 
   return (
     <React.StrictMode>
@@ -19,13 +33,7 @@ const App = () => {
         <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
         <button onClick={() => setBad(bad + 1)}>bad</button>
 
-        <h2>statistics</h2>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average}</p>
-        <p>positive {positive} %</p>
+        <Statistics good={good} neutral={neutral} bad={bad} />
       </React.Fragment>
     </React.StrictMode>
   );
