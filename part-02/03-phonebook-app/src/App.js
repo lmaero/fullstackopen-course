3a1b4d452b94eaa3d735bf1369fc49e16331093a
Luis Guzman <luis.dev@lmaero.pro>
2021-06-15 19:35:33 -0500

    Complete exercise 2.6

---
+++ part-02/03-phonebook-app/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const addPerson = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <React.StrictMode>
+      <React.Fragment>
+        <h1>Phonebook App</h1>
+
+        <h2>Phonebook</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name:{' '}
+            <input autoFocus value={newName} onChange={handleNameChange} />
+          </div>
+          <div>
+            <button type='submit'>add</button>
+          </div>
+        </form>
+
+        <h2>Numbers</h2>
+        {persons.map((person) => (
+          <p key={person.name}>{person.name}</p>
+        ))}
+      </React.Fragment>
+    </React.StrictMode>
+  );
+};
+
+export default App;
