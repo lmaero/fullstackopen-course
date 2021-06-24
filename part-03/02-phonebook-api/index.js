9ec7e975a6317ca9b3edf949a667de275829e7c6
Luis Guzman <luis.dev@lmaero.pro>
2021-06-24 07:00:44 -0500

    Fix persons array IDs 2 and 3 were missing

--- part-03/02-phonebook-api/index.js
+++ part-03/02-phonebook-api/index.js
@@ -10,12 +10,12 @@ const persons = [
     number: '040-123456',
   },
   {
-    id: 1,
+    id: 2,
     name: 'Ada Lovelace',
     number: '39-44-5323523',
   },
   {
-    id: 1,
+    id: 3,
     name: 'Dan Abramov',
     number: '12-43-234345',
   },
