b7e597a0289cc7c2acb355ece0e35799f251ee5a
Luis Guzman <luis.dev@lmaero.pro>
2021-06-25 10:57:10 -0500

    Add Procfile and configure PORT for phonebook-api

--- part-03/02-phonebook-api/index.js
+++ part-03/02-phonebook-api/index.js
@@ -99,6 +99,7 @@ app.post('/api/persons/', (request, response) => {
   response.status(201).json(newPerson);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Phonebook API running on port ${3001}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Phonebook API running on port ${3001}`);
+});
