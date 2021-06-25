030174f0d4aa8ce70f2f2add3ea4fc07b50e66f1
Luis Guzman <luis.dev@lmaero.pro>
2021-06-25 16:45:36 -0500

    Complete exercise 3.13

--- part-03/02-phonebook-api/index.js
+++ part-03/02-phonebook-api/index.js
@@ -1,7 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
+
+const Person = require('./models/person');
 
 app.use(cors());
 app.use(express.json());
@@ -17,47 +20,26 @@ app.use(
   ),
 );
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: 4,
-    name: 'Mary Poppendick',
-    number: '39-23-6423122',
-  },
-];
-
 app.get('/info', (request, response) => {
-  response.send(`
-    <h1>Phonebook API Info</h1>
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-    `);
+  Person.find({}).then((persons) => {
+    response.send(`
+      <h1>Phonebook API Info</h1>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+      `);
+  });
 });
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const personId = Number(request.params.id);
-  const person = persons.find((person) => person.id === personId);
-
-  if (person) response.json(person);
-  else response.status(404).end();
+  Person.findById(request.params.id).then((person) => {
+    response.json(person);
+  });
 });
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -100,7 +82,7 @@ app.post('/api/persons/', (request, response) => {
   response.status(201).json(newPerson);
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Phonebook API running on port ${3001}`);
+  console.log(`Phonebook API running on port ${PORT}`);
 });
---
+++ part-03/02-phonebook-api/models/person.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+console.log(`Connecting to ${url}`);
+
+mongoose
+  .connect(url, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log(`Something went wrong: ${error.message}`);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Person', personSchema);
