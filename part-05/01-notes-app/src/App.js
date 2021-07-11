505e753eb936fd984ba5f19b305a6253c6101d1c
Luis Guzman <luis.dev@lmaero.pro>
2021-07-11 07:34:03 -0500

    Add part 5.b code from theory before exercise 5.5

--- part-05/01-notes-app/src/App.js
+++ part-05/01-notes-app/src/App.js
@@ -1,19 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Footer from './components/Footer';
+import LoginForm from './components/LoginForm';
 import Note from './components/Note';
+import NoteForm from './components/NoteForm';
 import Notification from './components/Notification';
+import Togglable from './components/Togglable';
 import loginService from './services/login';
 import noteService from './services/notes';
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
 
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+
+  const noteFormRef = useRef();
 
   useEffect(() => {
     noteService
@@ -30,20 +34,12 @@ const App = () => {
     }
   }, []);
 
-  const addNote = (event) => {
-    event.preventDefault();
-
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-    };
-
+  const addNote = (noteObject) => {
+    noteFormRef.current.toggleVisibility();
     noteService
       .create(noteObject)
       .then((returnedNote) => {
         setNotes(notes.concat(returnedNote));
-        setNewNote('');
       });
   };
 
@@ -65,10 +61,6 @@ const App = () => {
         );
         setTimeout(() => { setErrorMessage(null); }, 5000);
       });
-  };
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value);
   };
 
   const notesToShow = showAll
@@ -97,50 +89,22 @@ const App = () => {
     }
   };
 
-  const handleLogout = (event) => {
-    event.preventDefault();
-
-    window.localStorage.removeItem('loggedNoteappUser');
-    setUser(null);
-    setErrorMessage('Successfully logged out');
-    setTimeout(() => { setErrorMessage(null); }, 5000);
-  };
-
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        {'username '}
-        <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-
-      <div>
-        {'password '}
-        <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-
-      <button type="submit">login</button>
-    </form>
+    <Togglable buttonLabel="log in">
+      <LoginForm
+        username={username}
+        password={password}
+        handleUsernameChange={({ target }) => setUsername(target.value)}
+        handlePasswordChange={({ target }) => setPassword(target.value)}
+        handleSubmit={handleLogin}
+      />
+    </Togglable>
   );
 
   const noteForm = () => (
-    <form onSubmit={addNote}>
-      <input
-        value={newNote}
-        onChange={handleNoteChange}
-      />
-
-      <button type="submit">save</button>
-    </form>
+    <Togglable buttonLabel="new note" ref={noteFormRef}>
+      <NoteForm createNote={addNote} />
+    </Togglable>
   );
 
   return (
@@ -154,16 +118,14 @@ const App = () => {
             ? loginForm()
             : (
               <div>
-                <p>{`${user.name} logged-in`}</p>
-                <button
-                  type="button"
-                  onClick={handleLogout}
-                >
-                  log out
-                </button>
+                <p>
+                  {user.name}
+                  {' '}
+                  logged in
+                </p>
                 {noteForm()}
               </div>
-            ) }
+            )}
 
           <div>
             <button
