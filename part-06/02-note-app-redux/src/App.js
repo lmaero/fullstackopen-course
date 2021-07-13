d2139b9e0968fdaed08d686571552660b6ee44e3
Luis Guzman <luis.dev@lmaero.pro>
2021-07-13 17:14:39 -0500

    Add part 6.a code from theory before exercise 6.3

--- part-06/02-note-app-redux/src/App.js
+++ part-06/02-note-app-redux/src/App.js
@@ -1,49 +1,13 @@
 import React from 'react';
-import { createStore } from 'redux';
-import noteReducer from './reducers/noteReducer';
+import NewNote from './components/NewNote';
+import Notes from './components/Notes';
 
-const store = createStore(noteReducer);
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1,
-  },
-});
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2,
-  },
-});
-
-store.dispatch({
-  type: 'TOGGLE_IMPORTANCE',
-  data: {
-    id: 2,
-  },
-});
-
-const App = (props) => {
+const App = () => {
   return (
-    <React.StrictMode>
-      <React.Fragment>
-        <h1>Notes-App Redux</h1>
-        <ul>
-          {store.getState().map((note) => (
-            <li key={note.id}>
-              {note.content}{' '}
-              <strong>{note.important ? 'important' : ''}</strong>
-            </li>
-          ))}
-        </ul>
-      </React.Fragment>
-    </React.StrictMode>
+    <div>
+      <NewNote />
+      <Notes />
+    </div>
   );
 };
 
