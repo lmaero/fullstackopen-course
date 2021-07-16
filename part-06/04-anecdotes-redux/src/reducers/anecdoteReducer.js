280f000bb69179bd1838170b558751655731a68b
Luis Guzman <luis.dev@lmaero.pro>
2021-07-16 12:55:45 -0500

    Complete exercise 6.15

--- part-06/04-anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part-06/04-anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import anecdoteService from '../services/anecdotes';
+
 const reducer = (state = [], action) => {
   switch (action.type) {
     case 'VOTE':
@@ -39,10 +41,13 @@ export const createAnecdote = (data) => {
   };
 };
 
-export const initializeAnecdotes = (anecdotes) => {
-  return {
-    type: 'INIT_ANECDOTES',
-    data: anecdotes,
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch({
+      type: 'INIT_ANECDOTES',
+      data: anecdotes,
+    });
   };
 };
 
