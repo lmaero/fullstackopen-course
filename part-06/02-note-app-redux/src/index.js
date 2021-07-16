406d8d69956824507fec813f32e0f96bb54170d0
Luis Guzman <luis.dev@lmaero.pro>
2021-07-16 10:04:08 -0500

    Add part 6.b code from theory before exercise 6.9

--- part-06/02-note-app-redux/src/index.js
+++ part-06/02-note-app-redux/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import App from './App';
+import filterReducer from './reducers/filterReducer';
 import noteReducer from './reducers/noteReducer';
 
-const store = createStore(noteReducer);
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
 
+const store = createStore(reducer, composeWithDevTools());
+
+console.log(store.getState());
 ReactDOM.render(
   <Provider store={store}>
     <App />
