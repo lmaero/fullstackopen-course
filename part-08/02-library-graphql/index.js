076db1e0ce698fa03fb5ed49b3d583aa8054d6c3
Luis Guzman <luis.dev@lmaero.pro>
2021-08-03 19:26:58 -0500

    Complete exercise 8.4

--- part-08/02-library-graphql/index.js
+++ part-08/02-library-graphql/index.js
@@ -94,7 +94,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book]
+    allBooks(author: String): [Book]
     allAuthors: [Author]
   }
 `;
@@ -103,7 +103,7 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => books.filter((b) => b.author === args.author),
     allAuthors: () => authors,
   },
   Author: {
